04da67fa349a82d569dfcbf17af821832cc6ad8d
mangelgl <magarcial.dev@outlook.es>
2022-09-25 01:41:34 +0200

    02_Day_Data_Types

--- 02_Day_Data_Types/scripts/ejercicios3.js
+++ 02_Day_Data_Types/scripts/ejercicios3.js
@@ -18,8 +18,12 @@ let pattern1 = /because/g;
 console.log("2 ->", "You cannot end a sentence with because because because is a conjunction".match(pattern1));
 
 const sentence =
-  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
-console.log("3 ->");
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!?%Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+let pattern3 = /[%$@#&;]*/g;
+let sentenceReplace = sentence.replace(pattern3, "");
+let pattern4 = /[^\s.,?!][\w]*/g;
+let result = sentenceReplace.match(pattern4);
+console.log("3 ->", result);
 
 let pattern2 = /\d+/g;
 let digits = "He earns 5000 euro from salary per month, 10000 euro annual bonus, 1500 euro online courses per month.".match(pattern2);
